142cfa3933f473552d4ecc3d0a100b326f07d15a
LingannaJ <jlinganna104@gmail.com>
2025-04-09 12:41:54 +0530

    shell

---
+++ shell.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+# Defining Numbers
+start=1
+end=20
+
+echo "Printing the odd numbers from $start to $end are:"
+
+# Use a for loop
+for (( i=start; i<=end; i++ ))
+do
+    if (( i % 2 != 0 )); then
+        echo -n "$i "
+    fi
+done
+
+echo -e "\nDone!"
+
+
+
+
+
